0a5f767f4a4f7143f41a1c71318facb525f36da7
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2020-11-28 10:42:08 -0500

    Update Relationship and creating table.sql

--- Relationship and creating table.sql
+++ Relationship and creating table.sql
@@ -1,49 +1,69 @@
-CREATE TABLE "department" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "dept_name" VARCHAR(100)   NOT NULL
-);
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "department_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL
-);
 
-CREATE TABLE "department_mgr" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "emp_no" INT   NOT NULL
-);
+SET XACT_ABORT ON
 
-CREATE TABLE "employee" (
-    "emp_no" INT   NOT NULL,
-    "emp_title" VARCHAR(5)   NOT NULL,
-    "birth_date" VARCHAR(50)   NOT NULL,
-    "first_name" VARCHAR(100)   NOT NULL,
-    "last_name" VARCHAR(100)   NOT NULL,
-    "sex" VARCHAR(2)   NOT NULL,
-    "hire_date" VARCHAR(50) NOT NULL
-);
+BEGIN TRANSACTION QUICKDBD
 
-CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
-);
+CREATE TABLE [Department_emp] (
+    [emp_no] INT  NOT NULL ,
+    [dept_no] VARCHAR(4)  NOT NULL 
+)
 
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(5)   NOT NULL,
-    "title" VARCHAR(25)   NOT NULL
-	
-ALTER TABLE "department_emp" ADD CONSTRAINT "fk_department_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
+CREATE TABLE [Department] (
+    [dept_no] VARCHAR(4)  NOT NULL ,
+    [dept_name] VARCHAR(100)  NOT NULL 
+)
 
-ALTER TABLE "department_mgr" ADD CONSTRAINT "fk_department_mgr_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
+CREATE TABLE [Department_mgr] (
+    [dept_no] VARCHAR(4)  NOT NULL ,
+    [emp_no] INT  NOT NULL 
+)
 
-ALTER TABLE "department_mgr" ADD CONSTRAINT "fk_department_mgr_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employee" ("emp_no");
+CREATE TABLE [Employee] (
+    [emp_no] INT  NOT NULL ,
+    [emp_title] VARCHAR(5)  NOT NULL ,
+    [birth_date] VARCHAR(50)  NOT NULL ,
+    [first_name] VARCHAR(100)  NOT NULL ,
+    [last_name] VARCHAR(100)  NOT NULL ,
+    [sex] VARCHAR(2)  NOT NULL ,
+    [hire_date] VARCHAR(50)  NOT NULL 
+)
 
-ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "salaries" ("emp_no");
+CREATE TABLE [Salaries] (
+    [emp_no] INT  NOT NULL ,
+    [salary] INT  NOT NULL 
+)
 
-ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "titles" ("title_id");
+CREATE TABLE [Titles] (
+    [title_id] VARCHAR(5)  NOT NULL ,
+    [title] VARCHAR(25)  NOT NULL 
+)
 
+ALTER TABLE [Department_emp] WITH CHECK ADD CONSTRAINT [FK_Department_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [Department] ([dept_no])
+
+ALTER TABLE [Department_emp] CHECK CONSTRAINT [FK_Department_emp_dept_no]
+
+ALTER TABLE [Department] WITH CHECK ADD CONSTRAINT [FK_Department_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [Department_mgr] ([dept_no])
+
+ALTER TABLE [Department] CHECK CONSTRAINT [FK_Department_dept_no]
+
+ALTER TABLE [Department_mgr] WITH CHECK ADD CONSTRAINT [FK_Department_mgr_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [Employee] ([emp_no])
+
+ALTER TABLE [Department_mgr] CHECK CONSTRAINT [FK_Department_mgr_emp_no]
+
+ALTER TABLE [Employee] WITH CHECK ADD CONSTRAINT [FK_Employee_emp_title] FOREIGN KEY([emp_title])
+REFERENCES [Titles] ([title_id])
+
+ALTER TABLE [Employee] CHECK CONSTRAINT [FK_Employee_emp_title]
+
+ALTER TABLE [Salaries] WITH CHECK ADD CONSTRAINT [FK_Salaries_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [Employee] ([emp_no])
+
+ALTER TABLE [Salaries] CHECK CONSTRAINT [FK_Salaries_emp_no]
+
+COMMIT TRANSACTION QUICKDBD
